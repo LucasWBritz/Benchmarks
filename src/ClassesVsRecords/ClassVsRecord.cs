734b0d25c8c3aa2fd5acfcea721399e58b268e67
LucasWBritz <lucas.wi.britz@gmail.com>
2021-07-16 12:07:46 -0300

    Add new benchmark

---
+++ src/ClassesVsRecords/ClassVsRecord.cs
@@ -0,0 +1,41 @@
+﻿using BenchmarkDotNet.Attributes;
+using System.Collections.Generic;
+
+namespace ClassesVsRecords
+{
+    [MemoryDiagnoser]
+    public class ClassVsRecord
+    {
+        [Benchmark]
+        public void CreatePerson()
+        {
+            List<Person> persons = new();
+            for (int i = 0; i < 1000; i++)
+            {
+                persons.Add(new Person()
+                {
+                    Name = i.ToString(),
+                    Age = i
+                });
+            }
+        }
+
+        [Benchmark]
+        public void CreatePersonRecord()
+        {
+            List<PersonRecord> persons = new();
+            for (int i = 0; i < 1000; i++)
+            {
+                persons.Add(new PersonRecord(i.ToString(), i));
+            }
+        }
+    }
+
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+
+    public record PersonRecord(string Name, int Age);
+}
---
+++ src/ClassesVsRecords/Program.cs
@@ -0,0 +1,15 @@
+﻿using BenchmarkDotNet.Running;
+using System;
+
+namespace ClassesVsRecords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<ClassVsRecord>();
+
+            Console.ReadKey();
+        }
+    }
+}
