1dff8eaa84aeb1073b6a595e327c21bad4bc1ad2
LucasWBritz <lucas.wi.britz@gmail.com>
2021-07-26 17:21:55 -0300

    Add paralell operations

---
+++ src/ParalellOperations/ParalellOperation.cs
@@ -0,0 +1,50 @@
+﻿using BenchmarkDotNet.Attributes;
+using System.Linq;
+
+namespace ParalellOperations
+{
+    [MemoryDiagnoser]
+    public class ParalellOperation
+    {
+        public static readonly int[] BillionValues = Enumerable.Range(1, 9999).Select(x => x).ToArray();
+
+        [Benchmark]
+        public void SequentialForeach()
+        {
+            int even = 0;
+            foreach (var item in BillionValues)
+            {
+                if(item % 2 == 0)
+                {
+                    even++;
+                }
+            }
+        }
+
+
+        [Benchmark]
+        public void SequentialFor()
+        {
+            int even = 0;
+            for (int i = 0; i < BillionValues.Length; i++)
+            {
+                if (BillionValues[i] % 2 == 0)
+                {
+                    even++;
+                }
+            }            
+        }
+
+        [Benchmark]
+        public void ParallelLinq()
+        {
+            int even = BillionValues.AsParallel().Where(x => x % 2 == 0).Count();
+        }
+
+        [Benchmark]
+        public void Linq()
+        {
+            int even = BillionValues.Where(x => x % 2 == 0).Count();
+        }
+    }
+}
---
+++ src/ParalellOperations/Program.cs
@@ -0,0 +1,17 @@
+﻿using BenchmarkDotNet.Running;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ParalellOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<ParalellOperation>();
+
+            Console.ReadKey();
+        }
+    }
+}
